ad0ec0f6c99eace9738853ab8c6774aa6c26d778
OneJumpMan <sjw1999@byui.edu>
2023-06-14 09:48:36 -0600

    added minigame Cosmic Conquerors

---
+++ Cosmic Conquerors/Assets/Scripts/Conqueror.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Conqueror : MonoBehaviour {
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private int _animationFrame;
+
+    public System.Action killed;
+
+    private void Awake() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Laser")) {
+            this.killed.Invoke();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
+
+
